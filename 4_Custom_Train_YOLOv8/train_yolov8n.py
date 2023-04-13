612c0b41ccee6a04ec6743901fd6f15db4aceb48
Mukilan Krishnakumar <71809485+Mukilan-Krishnakumar@users.noreply.github.com>
2023-04-14 00:02:38 +0530

    Update train_yolov8n.py

--- 4_Custom_Train_YOLOv8/train_yolov8n.py
+++ 4_Custom_Train_YOLOv8/train_yolov8n.py
@@ -5,4 +5,4 @@ from wandb.integration.yolov8 import add_callbacks as add_wandb_callbacks
 model = YOLO("yolov8n.yaml")
 add_wandb_callbacks(model, project = "wildlife-yolov8")
 
-results = model.train(data = "wildlife_dataset.yaml",epochs = 1000, device = 0, save_period = 10)
+results = model.train(data = "../wildlife_dataset.yaml",epochs = 1000, device = 0, save_period = 10)
